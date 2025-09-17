8f72a139dd4f6791fab546148a839d6eb224295a
Asep5K <akunreza292@gmail.com>
2025-09-18 03:51:42 +0700

    from rofi to yad

--- yt.sh
+++ yt.sh
@@ -17,18 +17,14 @@
 # ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗
 # ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║
 # ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-
-# 🎨 Themes
-theme1="$HOME/.config/rofi/themes/yt-dlp.rasi"
-theme2="$HOME/.config/rofi/themes/list.rasi"
-f="/var/lib/AccountsService/icons/$USER"
-
+# export PATH="$PATH:$HOME/.cargo/bin:$HOME/.local/bin"
 ICON_SUCCESS_URL="https://pbs.twimg.com/profile_images/1937897644816265216/R9fhEWX3_400x400.jpg"
 ICON_FAIL_URL="https://pbs.twimg.com/media/Gzho6tiXUAAEXyj?format=jpg&name=medium"
 ICON_DIR="$HOME/.local/share/asep_icons"
 ICON_SUCCESS="$ICON_DIR/icon_success.jpg"
 ICON_FAIL="$ICON_DIR/icon_fail.jpg"
-
+LOG_DIR=/tmp/yt
+LOGFILE=yt.log
 # Pastikan folder ada
 [ ! -d "$ICON_DIR" ] && mkdir -p "$ICON_DIR"
 
@@ -36,7 +32,7 @@ ICON_FAIL="$ICON_DIR/icon_fail.jpg"
 [ ! -f "$ICON_SUCCESS" ] && wget -q -O "$ICON_SUCCESS" "$ICON_SUCCESS_URL"
 [ ! -f "$ICON_FAIL" ] && wget -q -O "$ICON_FAIL" "$ICON_FAIL_URL"
 
-trap 'pkill -P $$ mpv yt-dlp' EXIT
+# trap 'pkill -P $$ mpv; pkill -P $$ yt-dlp' EXIT
 
 check_internet() {
     if ! ping -c 1 8.8.8.8 >/dev/null 2>&1; then
@@ -53,11 +49,14 @@ if ! command -v yt-dlp >/dev/null 2>&1; then
 else
     check_internet
     # 📥 Input URL
-    url=$(rofi -dmenu -p "Enter YouTube URL:" -theme "$theme1")
+    url=$(yad --entry \
+    --title="Enter YouTube URL" \
+    --text="Paste YouTube link here:" \
+    --width=500)
 fi
 
-if pidof rofi > /dev/null; then
-    pkill rofi
+if pidof yad > /dev/null; then
+    pkill yad
 fi
 
 # 🔎 Cek dan validasi URL
@@ -69,39 +68,102 @@ elif [[ ! "$url" =~ ^https?://(www\.)?(youtube\.com|youtu\.be)/ ]]; then
 fi
 
 # 📌 Menu options
-options="Download Videos\nDownload Music\nPlay Music/Videos"
-choice=$(echo -e "$options" | rofi -dmenu -p "Select option:" -theme "$theme1")
-[ -z "$choice" ] && exit 0
+    options=$(yad --list \
+        --title="Download Options" \
+        --column="Option" \
+        "Download Videos" \
+        "Download Music" \
+        "Play Music/Videos" \
+        "Download Playlist Flac" \
+        --width=600 --height=200 \
+        --button=ok:0 \
+        --button=cancel:1 \
+        --print-column=1)
+    options="${options%|}"
+    if [[ -z "$options" ]]; then
+        echo "User canceled"
+        exit 0
+    fi
 
-download_videos() {
-    download_options="Download 240p\nDownload 360p\nDownload 480p\nDownload 720p\nDownload 1080p\nDownload 2K\nDownload 4K\nBest Resolution\nExit"
-    download_choice=$(echo -e "$download_options" | rofi -dmenu -p "Select option:" -theme "$theme1")
-    [ -z "$download_choice" ] && exit 0
+# Log
+log() {
+    mkdir -p "$LOG_DIR"
+    > "$LOG_DIR/$LOGFILE"
+    # yt-dlp --newline "$URL" > "$LOG_DIR/$LOGFILE" 2>&1 &
+    # yad --progress --title="Downloading..." --text="Please wait..." --percentage=0 --auto-close --file="$LOG_DIR/$LOGFILE"
+    # (
+    #   while IFS= read -r line; do
+    #     # parse persentase dari yt-dlp output
+    #     percent=$(echo "$line" | grep -oP '\d+(?=%)' | head -n1)
+    #     [ -n "$percent" ] && echo $percent
+    #   done < <(tail -f "$LOG_DIR/$LOGFILE")
+    # ) | yad --progress --title="Downloading..." --auto-close --percentage=0
+    yad --progress-teks="$LOG_DIR/$LOGFILE"
 }
 
+# Download videos
+download_videos() {
+    download_videos=$(yad --list --title="Video options" --column=Option \
+        "Download 240p" "Download 360p" "Download 480p" \
+        "Download 720p" "Download 1080p" "Download 2K" \
+        "Download 4K" "Best Resolution" "Exit" \
+        --width=600 --height=320 \
+        --button=ok:0 --button=cancel:1 \
+        --print-column=1)
+    download_videos="${download_videos%|}"
+    if [[ -z "$download_videos" ]]; then
+        echo "User canceled"
+        exit 0
+    fi
+}
 play() {
-    play_options="Play Music\nPlay 240p\nPlay 360p\nPlay 480p\nPlay 720p\nPlay 1080p\nPlay 2K\nPlay 4K\nBest Resolution\nExit"
-    play_choice=$(echo -e "$play_options" | rofi -dmenu -p "Select option" -theme "$theme1")
-    [ -z "$play_choice" ] && exit 0
+    play=$(yad --list --title="Play options" --column=Option \
+        "Play Music" "Play 240p" "Play 360p" "Play 480p" \
+        "Play 720p" "Play 1080p" "Play 2K" \
+        "Play 4K" "Best Resolution" "Exit" \
+        --width=600 --height=320 \
+        --button=ok:0 --button=cancel:1 \
+        --print-column=1)
+    play="${play%|}"
+    if [[ -z "$play" ]]; then
+        echo "User canceled"
+        exit 0
+    fi
 }
 
 music_download() {
-    music_options="Download Mp3\nDownload Playlist Mp3\nDownload Flac\nDownload Playlist Flac\nExit"
-    music_choice=$(echo -e "$music_options" | rofi -dmenu -p "Select option:" -theme "$theme1")
-    [ -z "$music_choice" ] && exit 0
+    music_download=$(yad --list --title="Play options" --column=Option \
+        "Download Mp3" "Download Playlist Mp3" \
+        "Download Flac" "Download Playlist Flac" "Exit" \
+        --width=600 --height=320 \
+        --button=ok:0 --button=cancel:1 \
+        --print-column=1)
+    music_download="${music_download%|}"
+    if [[ -z "$music_download" ]]; then
+        echo "User canceled"
+        exit 0
+    fi
 }
 
 # 🎵 Audio only
 music() {
-    filename="%(title)s.%(ext)s"
-    playlistname="%(playlist_index)s - %(title)s.%(ext)s"
     outdir="$HOME/Music/Downloads"
+    filename="%(title)s.%(ext)s"
+    playlistname="%(playlist_index)02d - %(title)s.%(ext)s"
+    renamefile() {
+        for f in "$outdir"/*.{flac,mp3}; do
+            [ -e "$f" ] || continue
+            new=$(echo "$f" | sed -E 's/^0([0-9]{2})/\1/')
+            mv "$f" "$new"
+        done
+    }
     mkdir -p "$outdir"
     if [ "$1" = "mp3" ]; then
+        # log &
         notify-send -i "$ICON_SUCCESS" "Downloading..." "Audio Mp3"
         if yt-dlp -x --audio-format mp3 \
             -o "$outdir/$filename" \
-            "$url"; then
+            "$url"; then #> "$LOG_DIR/$LOGFILE" 2>&1; then
             latest_music=$(ls -t "$outdir" | head -n 1)
             notify-send -i "$ICON_SUCCESS" "$latest_music" "Download complete"
         else 
@@ -109,7 +171,7 @@ music() {
         fi
     elif [ "$1" = "flac" ]; then
         notify-send -i "$ICON_SUCCESS" "Downloading..." "Audio Flac"
-        if yt-dlp -x --audio-format flac \
+        if  yt-dlp -x --audio-format flac \
             -o "$outdir/$filename" \
             "$url"; then
             latest_music=$(ls -t "$outdir" | head -n 1)
@@ -121,10 +183,11 @@ music() {
         notify-send -i "$ICON_SUCCESS" "Downloading Playlist Mp3"
         if yt-dlp --continue \
             -f bestaudio -x --audio-format mp3 \
-            -o "$outdir/$playlistname" \
+            -o "$outdir"/"$playlistname" \
             "$url"; then
             latest_music=$(ls -t "$outdir" | head -n 1)
             notify-send -i "$ICON_SUCCESS" "$latest_music" "✔ Download Finished"
+            # renamefile
         else
             notify-send -i "$ICON_FAIL" "❌ Download Failed" "Check the URL or network"
         fi
@@ -132,10 +195,11 @@ music() {
         notify-send -i "$ICON_SUCCESS" "Downloading Playlist Flac"
         if yt-dlp --continue \
             -f bestaudio -x --audio-format flac \
-            -o "$outdir/$playlistname" \
+            -o "$outdir"/"$playlistname" \
             "$url"; then
             latest_music=$(ls -t "$outdir" | head -n 1)
             notify-send -i "$ICON_SUCCESS" "$latest_music" "✔ Download Finished"
+            # renamefile
         else 
             notify-send -i "$ICON_FAIL" "❌ Download Failed" "Check the URL or network"
         fi
@@ -180,10 +244,10 @@ play_select() {
       fi
 }
 
-if [ -n "$choice" ]; then
-    if [ "$choice" = "Download Videos" ]; then
+if [ -n "$options" ]; then
+    if [ "$options" = "Download Videos" ]; then
         download_videos
-        case "$download_choice" in
+        case "$download_videos" in
             "Download 240p") download 240 ;;
             "Download 360p") download 360 ;;
             "Download 480p") download 480 ;;
@@ -194,9 +258,9 @@ if [ -n "$choice" ]; then
             "Best Resolution") download "best" ;;
             "Exit") exit 0 ;;
          esac
-    elif [ "$choice" = "Play Music/Videos" ]; then
+    elif [ "$options" = "Play Music/Videos" ]; then
             play
-        case "$play_choice" in
+        case "$play" in
             "Play Music") play_select "music" ;;
             "Play 240p") play_select 240 ;;
             "Play 360p") play_select 360 ;;
@@ -208,9 +272,9 @@ if [ -n "$choice" ]; then
             "Best Resolution") play_select "best" ;;
             "Exit") exit 0 ;;
         esac  
-    elif [ "$choice" = "Download Music" ]; then
+    elif [ "$options" = "Download Music" ]; then
             music_download
-        case "$music_choice" in
+        case "$music_download" in
             "Download Mp3") music "mp3" ;;
             "Download Playlist Mp3") music "playlistmp3" ;;
             "Download Flac") music "flac" ;;
